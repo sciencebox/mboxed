4c5e2fdbe39a5a11ccef94c370bfef7b60bb02f0
Enrico Bocchi <enrico.bocchi@cern.ch>
2021-09-06 11:21:22 +0200

    ScienceBox: Use Helm charts for sciencebox, eos, swan

--- ScienceBox.sh
+++ ScienceBox.sh
@@ -14,27 +14,21 @@ configure_network
 configure_gpu_support
 
 # Restart Docker after network (and GPU) changes
-echo "Restarting Docker..."
 restart_docker
  
-# Get Helm charts for EOS
-#get_git_repo $HELM_CHARTS_EOS
-
-# Get Helm charts for SWAN
-#get_git_repo $HELM_CHARTS_SWAN
-
-## # Get Docker images
-## pre_pull_images
+# Set up repositories for charts
+helm_repo_add sciencebox https://registry.cern.ch/chartrepo/sciencebox
+helm_repo_add eos https://registry.cern.ch/chartrepo/eos
+#helm_repo_add cernbox ???
+helm_repo_add swan https://registry.cern.ch/chartrepo/swan
+helm_repo_add sciencebox https://registry.cern.ch/chartrepo/sciencebox
+helm_repo_update
 
 # Prepare persistent storage
-create_persistent_storage
+# create_persistent_storage
 
 # Bootstrap Minikube 
-start_minikube
-#label_node
-
-## Configure Helm
-#configure_helm	# Not needed with Helm v3
+minikube_start
 
 ## # Deployment
 ## check_required_images
@@ -43,17 +37,4 @@ start_minikube
 
 # Deploy services with Helm charts
 #TODO: deploy_helm_charts
-
-echo "
-
-
-The eos-chart repo is not (yet) public.
-Please, run what follows one you have a valid KRB5 token:
-  git clone https://:@gitlab.cern.ch:8443/eos/eos-charts.git
-  cd eos-charts
-  git checkout enbodev
-  git pull
-  
-  ./install.sh
-"
 
