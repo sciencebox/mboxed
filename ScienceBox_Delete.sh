df0099c1a85a4a136f0ec48be5d8822ea4dbac23
Enrico Bocchi <enrico.bocchi@cern.ch>
2021-11-10 16:41:55 +0100

    scibox: Delete script should not use kuboxed

--- ScienceBox_Delete.sh
+++ ScienceBox_Delete.sh
@@ -9,19 +9,11 @@ need_root
 echo "WARNING: This will stop all the ScienceBox services and *DELETE* all data stored in ScienceBox (CERNBox files, Jupyter notebooks, etc.)."
 prompt_user_to_continue
 
-# Get kuboxed
-get_git_repo $KUBOXED_GIT
-prepare_kuboxed
-
-# Stop services first
+# Stop ScienceBox
 minikube_stop
+
+# Delete minikube installation
 minikube_delete
-
-# Delete pulled images to reclaim space
-delete_images
-
-# Delete stored configs, users' files, etc.
-delete_persistent_storage
 
 # Restore previous config if the user wants
 suggest_iptables_restore
