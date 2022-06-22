2bb4b16e630f5471de9fafbbfa8ab294a8e7920f
Jimil <jimildesai42@gmail.com>
2022-06-22 14:25:59 +0200

    changes in deployment scripts for swan
    Signed-off-by: Jimil <jimildesai42@gmail.com>

---
+++ Charts.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Import variables and functions
+source etc/common.sh
+source etc/deploy.sh
+
+# Deploy ScienceBox
+install_charts
--- etc/common.sh
+++ etc/common.sh
@@ -3,8 +3,8 @@
 
 
 # Environment
-HOSTNAME=$(hostname --fqdn)
-#HOSTNAME='jimil-nuc.dyndns.cern.ch'
+#HOSTNAME=$(hostname --fqdn)
+HOSTNAME='jimil-nuc.dyndns.cern.ch'
 CWD="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 # Software versions
